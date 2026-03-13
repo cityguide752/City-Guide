066fcb04f3d3bdb82bb58a72aadcfdf0a860e1e9
Kiran <cityguide752@gmail.com>
2026-03-13 12:00:00 +0530

    Set up navigation architecture and implement the central navigation graph.

---
+++ app/src/main/java/com/cityguide/app/navigation/NavGraph.kt
@@ -0,0 +1,55 @@
+package com.cityguide.app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cityguide.app.presentation.auth.AuthScreen
+import com.cityguide.app.presentation.home.HomeScreen
+import com.cityguide.app.presentation.splash.SplashScreen
+import com.cityguide.app.presentation.splash.SplashViewModel
+
+@Composable
+fun CityGuideNavGraph() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Splash
+    ) {
+
+        composable<Splash> {
+            val viewModel: SplashViewModel = viewModel()
+
+            SplashScreen(
+                viewModel = viewModel,
+                onNavigateToHome = {
+                    navController.navigate(Home) {
+                        popUpTo<Splash> { inclusive = true }
+                    }
+                },
+                onNavigateToAuth = {
+                    navController.navigate(Auth) {
+                        popUpTo<Splash> { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable<Auth> {
+            AuthScreen(
+                onAuthSuccess = {
+                    navController.navigate(Home) {
+                        popUpTo<Auth> { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable<Home> {
+            HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/cityguide/app/presentation/home/HomeScreen.kt
@@ -0,0 +1,4 @@
+package com.cityguide.app.presentation.home
+
+class HomeScreen {
+}
